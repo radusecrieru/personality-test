51a6052cc5154e2a39aa983701638a17b5222b4c
Radu Secrieru <radu.secrieru@mambu.com>
2023-05-13 13:11:22 +0300

    add flyway and include table structure and data

---
+++ db/migration/V0.0.2__questions_data.sql
@@ -0,0 +1,83 @@
+INSERT INTO QUESTIONS (ID, TEXT) VALUES (1, 'You’re really busy at work and a colleague is telling you their life story and personal woes. You:');
+INSERT INTO QUESTIONS_ANSWERS (QUESTION_ID, TEXT, PERSONALITY_SIGN) VALUES ( 1, 'Don’t dare to interrupt them', 'PRIVATE_INTROVERTED');
+INSERT INTO QUESTIONS_ANSWERS (QUESTION_ID, TEXT, PERSONALITY_SIGN) VALUES ( 1, 'Think it’s more important to give them some of your time; work can wait', 'PRIVATE_EXTROVERTED');
+INSERT INTO QUESTIONS_ANSWERS (QUESTION_ID, TEXT, PERSONALITY_SIGN) VALUES ( 1, 'Listen, but with only with half an ear', 'PUBLIC_INTROVERTED');
+INSERT INTO QUESTIONS_ANSWERS (QUESTION_ID, TEXT, PERSONALITY_SIGN) VALUES ( 1, 'Interrupt and explain that you are really busy at the moment', 'PUBLIC_EXTROVERTED');
+
+INSERT INTO QUESTIONS (ID, TEXT) VALUES (2, 'You’ve been sitting in the doctor’s waiting room for more than 25 minutes. You:');
+INSERT INTO QUESTIONS_ANSWERS (QUESTION_ID, TEXT, PERSONALITY_SIGN) VALUES ( 2, 'Look at your watch every two minutes', 'PRIVATE_INTROVERTED');
+INSERT INTO QUESTIONS_ANSWERS (QUESTION_ID, TEXT, PERSONALITY_SIGN) VALUES ( 2, 'Bubble with inner anger, but keep quiet', 'PRIVATE_EXTROVERTED');
+INSERT INTO QUESTIONS_ANSWERS (QUESTION_ID, TEXT, PERSONALITY_SIGN) VALUES ( 2, 'Explain to other equally impatient people in the room that the doctor is always running late', 'PUBLIC_INTROVERTED');
+INSERT INTO QUESTIONS_ANSWERS (QUESTION_ID, TEXT, PERSONALITY_SIGN) VALUES ( 2, 'Complain in a loud voice, while tapping your foot impatiently', 'PUBLIC_EXTROVERTED');
+
+INSERT INTO QUESTIONS (ID, TEXT) VALUES (3, 'You’re having an animated discussion with a colleague regarding a project that you’re in charge of. You:');
+INSERT INTO QUESTIONS_ANSWERS (QUESTION_ID, TEXT, PERSONALITY_SIGN) VALUES ( 3, 'Don’t dare contradict them', 'PRIVATE_INTROVERTED');
+INSERT INTO QUESTIONS_ANSWERS (QUESTION_ID, TEXT, PERSONALITY_SIGN) VALUES ( 3, 'Think that they are obviously right', 'PRIVATE_EXTROVERTED');
+INSERT INTO QUESTIONS_ANSWERS (QUESTION_ID, TEXT, PERSONALITY_SIGN) VALUES ( 3, 'Defend your own point of view, tooth and nail', 'PUBLIC_INTROVERTED');
+INSERT INTO QUESTIONS_ANSWERS (QUESTION_ID, TEXT, PERSONALITY_SIGN) VALUES ( 3, 'Continuously interrupt your colleague', 'PUBLIC_EXTROVERTED');
+
+INSERT INTO QUESTIONS (ID, TEXT) VALUES (4, 'You are taking part in a guided tour of a museum. You:');
+INSERT INTO QUESTIONS_ANSWERS (QUESTION_ID, TEXT, PERSONALITY_SIGN) VALUES ( 4, 'Are a bit too far towards the back so don’t really hear what the guide is saying', 'PRIVATE_INTROVERTED');
+INSERT INTO QUESTIONS_ANSWERS (QUESTION_ID, TEXT, PERSONALITY_SIGN) VALUES ( 4, 'Follow the group without question', 'PRIVATE_EXTROVERTED');
+INSERT INTO QUESTIONS_ANSWERS (QUESTION_ID, TEXT, PERSONALITY_SIGN) VALUES ( 4, 'Make sure that everyone is able to hear properly', 'PUBLIC_INTROVERTED');
+INSERT INTO QUESTIONS_ANSWERS (QUESTION_ID, TEXT, PERSONALITY_SIGN) VALUES ( 4, 'Are right up the front, adding your own comments in a loud voice', 'PUBLIC_EXTROVERTED');
+
+INSERT INTO QUESTIONS (ID, TEXT) VALUES (5, 'During dinner parties at your home, you have a hard time with people who:');
+INSERT INTO QUESTIONS_ANSWERS (QUESTION_ID, TEXT, PERSONALITY_SIGN) VALUES ( 5, 'Ask you to tell a story in front of everyone else ', 'PRIVATE_INTROVERTED');
+INSERT INTO QUESTIONS_ANSWERS (QUESTION_ID, TEXT, PERSONALITY_SIGN) VALUES ( 5, 'Talk privately between themselves', 'PRIVATE_EXTROVERTED');
+INSERT INTO QUESTIONS_ANSWERS (QUESTION_ID, TEXT, PERSONALITY_SIGN) VALUES ( 5, 'Hang around you all evening', 'PUBLIC_INTROVERTED');
+INSERT INTO QUESTIONS_ANSWERS (QUESTION_ID, TEXT, PERSONALITY_SIGN) VALUES ( 5, 'Always drag the conversation back to themselves', 'PUBLIC_EXTROVERTED');
+
+INSERT INTO QUESTIONS (ID, TEXT) VALUES (6, 'You crack a joke at work, but nobody seems to have noticed. You:');
+INSERT INTO QUESTIONS_ANSWERS (QUESTION_ID, TEXT, PERSONALITY_SIGN) VALUES ( 6, 'Think it’s for the best — it was a lame joke anyway', 'PRIVATE_INTROVERTED');
+INSERT INTO QUESTIONS_ANSWERS (QUESTION_ID, TEXT, PERSONALITY_SIGN) VALUES ( 6, 'Wait to share it with your friends after work', 'PRIVATE_EXTROVERTED');
+INSERT INTO QUESTIONS_ANSWERS (QUESTION_ID, TEXT, PERSONALITY_SIGN) VALUES ( 6, 'Try again a bit later with one of your colleagues', 'PUBLIC_INTROVERTED');
+INSERT INTO QUESTIONS_ANSWERS (QUESTION_ID, TEXT, PERSONALITY_SIGN) VALUES ( 6, 'Keep telling it until they pay attention', 'PUBLIC_EXTROVERTED');
+
+INSERT INTO QUESTIONS (ID, TEXT) VALUES (7, 'This morning, your agenda seems to be free. You:');
+INSERT INTO QUESTIONS_ANSWERS (QUESTION_ID, TEXT, PERSONALITY_SIGN) VALUES ( 7, 'Know that somebody will find a reason to come and bother you', 'PRIVATE_INTROVERTED');
+INSERT INTO QUESTIONS_ANSWERS (QUESTION_ID, TEXT, PERSONALITY_SIGN) VALUES ( 7, 'Heave a sigh of relief and look forward to a day without stress', 'PRIVATE_EXTROVERTED');
+INSERT INTO QUESTIONS_ANSWERS (QUESTION_ID, TEXT, PERSONALITY_SIGN) VALUES ( 7, 'Question your colleagues about a project that’s been worrying you', 'PUBLIC_INTROVERTED');
+INSERT INTO QUESTIONS_ANSWERS (QUESTION_ID, TEXT, PERSONALITY_SIGN) VALUES ( 7, 'Pick up the phone and start filling up your agenda with meetings', 'PUBLIC_EXTROVERTED');
+
+INSERT INTO QUESTIONS (ID, TEXT) VALUES (8, 'During dinner, the discussion moves to a subject about which you know nothing at all. You:');
+INSERT INTO QUESTIONS_ANSWERS (QUESTION_ID, TEXT, PERSONALITY_SIGN) VALUES ( 8, 'Don’t dare show that you don’t know anything about the subject', 'PRIVATE_INTROVERTED');
+INSERT INTO QUESTIONS_ANSWERS (QUESTION_ID, TEXT, PERSONALITY_SIGN) VALUES ( 8, 'Barely follow the discussion', 'PRIVATE_EXTROVERTED');
+INSERT INTO QUESTIONS_ANSWERS (QUESTION_ID, TEXT, PERSONALITY_SIGN) VALUES ( 8, 'Ask lots of questions to learn more about it', 'PUBLIC_INTROVERTED');
+INSERT INTO QUESTIONS_ANSWERS (QUESTION_ID, TEXT, PERSONALITY_SIGN) VALUES ( 8, 'Change the subject of discussion ', 'PUBLIC_EXTROVERTED');
+
+INSERT INTO QUESTIONS (ID, TEXT) VALUES (9, 'You’re out with a group of friends and there’s a person who’s quite shy and doesn’t talk much. You:');
+INSERT INTO QUESTIONS_ANSWERS (QUESTION_ID, TEXT, PERSONALITY_SIGN) VALUES ( 9, 'Notice that they’re alone, but don’t go over to talk with them', 'PRIVATE_INTROVERTED');
+INSERT INTO QUESTIONS_ANSWERS (QUESTION_ID, TEXT, PERSONALITY_SIGN) VALUES ( 9, 'Go and have a chat with them', 'PRIVATE_EXTROVERTED');
+INSERT INTO QUESTIONS_ANSWERS (QUESTION_ID, TEXT, PERSONALITY_SIGN) VALUES ( 9, 'Shoot some friendly smiles in their direction', 'PUBLIC_INTROVERTED');
+INSERT INTO QUESTIONS_ANSWERS (QUESTION_ID, TEXT, PERSONALITY_SIGN) VALUES ( 9, 'Hardly notice them at all', 'PUBLIC_EXTROVERTED');
+
+INSERT INTO QUESTIONS (ID, TEXT) VALUES (10, 'At work, somebody asks for your help for the hundredth time. You:');
+INSERT INTO QUESTIONS_ANSWERS (QUESTION_ID, TEXT, PERSONALITY_SIGN) VALUES ( 10, 'Give them a hand, as usual', 'PRIVATE_INTROVERTED');
+INSERT INTO QUESTIONS_ANSWERS (QUESTION_ID, TEXT, PERSONALITY_SIGN) VALUES ( 10, 'Accept — you’re known for being helpful', 'PRIVATE_EXTROVERTED');
+INSERT INTO QUESTIONS_ANSWERS (QUESTION_ID, TEXT, PERSONALITY_SIGN) VALUES ( 10, 'Ask them, please, to find somebody else for a change', 'PUBLIC_INTROVERTED');
+INSERT INTO QUESTIONS_ANSWERS (QUESTION_ID, TEXT, PERSONALITY_SIGN) VALUES ( 10, 'Loudly make it known that you’re annoyed', 'PUBLIC_EXTROVERTED');
+
+INSERT INTO QUESTIONS (ID, TEXT) VALUES (11, 'You’ve been see a movie with your family and the reviews are mixed. You:');
+INSERT INTO QUESTIONS_ANSWERS (QUESTION_ID, TEXT, PERSONALITY_SIGN) VALUES ( 11, 'Don’t share your point of view with anyone', 'PRIVATE_INTROVERTED');
+INSERT INTO QUESTIONS_ANSWERS (QUESTION_ID, TEXT, PERSONALITY_SIGN) VALUES ( 11, 'Didn’t like the film, but keep your views to yourself when asked', 'PRIVATE_EXTROVERTED');
+INSERT INTO QUESTIONS_ANSWERS (QUESTION_ID, TEXT, PERSONALITY_SIGN) VALUES ( 11, 'State your point of view with enthusiasm', 'PUBLIC_INTROVERTED');
+INSERT INTO QUESTIONS_ANSWERS (QUESTION_ID, TEXT, PERSONALITY_SIGN) VALUES ( 11, 'Try to bring the others round to your point of view', 'PUBLIC_EXTROVERTED');
+
+INSERT INTO QUESTIONS (ID, TEXT) VALUES (12, 'A friend arrives late for your meeting. You:');
+INSERT INTO QUESTIONS_ANSWERS (QUESTION_ID, TEXT, PERSONALITY_SIGN) VALUES ( 12, 'Say, ‘It’s not a problem,’ even if that’s not what you really think', 'PRIVATE_INTROVERTED');
+INSERT INTO QUESTIONS_ANSWERS (QUESTION_ID, TEXT, PERSONALITY_SIGN) VALUES ( 12, 'Give them a filthy look and sulk for the rest of the evening', 'PRIVATE_EXTROVERTED');
+INSERT INTO QUESTIONS_ANSWERS (QUESTION_ID, TEXT, PERSONALITY_SIGN) VALUES ( 12, 'Tell them, ‘You’re too much! Have you seen the time?’', 'PUBLIC_INTROVERTED');
+INSERT INTO QUESTIONS_ANSWERS (QUESTION_ID, TEXT, PERSONALITY_SIGN) VALUES ( 12, 'Make a scene in front of everyone', 'PUBLIC_EXTROVERTED');
+
+INSERT INTO QUESTIONS (ID, TEXT) VALUES (13, 'You can’t find your car keys. You:');
+INSERT INTO QUESTIONS_ANSWERS (QUESTION_ID, TEXT, PERSONALITY_SIGN) VALUES ( 13, 'Don’t want anyone to find out, so you take the bus instead', 'PRIVATE_INTROVERTED');
+INSERT INTO QUESTIONS_ANSWERS (QUESTION_ID, TEXT, PERSONALITY_SIGN) VALUES ( 13, 'Panic and search madly without asking anyone for help', 'PRIVATE_EXTROVERTED');
+INSERT INTO QUESTIONS_ANSWERS (QUESTION_ID, TEXT, PERSONALITY_SIGN) VALUES ( 13, 'Grumble without telling your family why you’re in a bad mood', 'PUBLIC_INTROVERTED');
+INSERT INTO QUESTIONS_ANSWERS (QUESTION_ID, TEXT, PERSONALITY_SIGN) VALUES ( 13, 'Accuse those around you for misplacing them', 'PUBLIC_EXTROVERTED');
+
+INSERT INTO QUESTIONS (ID, TEXT) VALUES (14, 'It’s time for your annual appraisal with your boss. You:');
+INSERT INTO QUESTIONS_ANSWERS (QUESTION_ID, TEXT, PERSONALITY_SIGN) VALUES ( 14, 'Go with great hesitation as these sessions are torture for you', 'PRIVATE_INTROVERTED');
+INSERT INTO QUESTIONS_ANSWERS (QUESTION_ID, TEXT, PERSONALITY_SIGN) VALUES ( 14, 'Look forward to hearing what your boss thinks about you and expects from you', 'PRIVATE_EXTROVERTED');
+INSERT INTO QUESTIONS_ANSWERS (QUESTION_ID, TEXT, PERSONALITY_SIGN) VALUES ( 14, 'Rehearse ad nauseam the arguments and ideas that you’ve prepared for the meeting', 'PUBLIC_INTROVERTED');
+INSERT INTO QUESTIONS_ANSWERS (QUESTION_ID, TEXT, PERSONALITY_SIGN) VALUES ( 14, 'Go along unprepared as you are confident and like improvising', 'PUBLIC_EXTROVERTED');
